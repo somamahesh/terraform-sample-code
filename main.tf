02d3b231855ed86b418081f4f757ac009060a207
somamahesh <37290341+somamahesh@users.noreply.github.com>
2022-09-23 12:04:17 +0530

    updated client

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "myawsserver" {
     env = upper (var.env)
     owner = upper ("Techlanders")
     dept = "Eng"
-    client = "vodafone"
+    client = "vodafone-1"
   }
 }
 
